332df97004d243cbc35859fa9b7e3a8560e9c293
zhaoxuan <zhaoxuan1727@gmail.com>
2014-08-21 11:39:57 +0800

    add file header to ruby script

--- compressor.rb
+++ compressor.rb
@@ -1,5 +1,9 @@
+# encoding=UTF-8
+# -*- coding: UTF-8 -*-
+# coding: UTF-8
+# Copyright © 2014 john <john@apple.local>
+
 require 'uglifier'
 
 compress_js = open('./dist/tryfer-min.js', 'w')
 compress_js.print(Uglifier.new.compile(File.read("./dist/tryfer.js")))
-compress_js.close
